7de8d20ae1634d8e397cd5e9d36ce8ed38385768
Rashedul Hasan <Rashedul Hasan>
2022-02-22 22:53:55 +0600

    Class-12

---
+++ index.js
@@ -0,0 +1,41 @@
+// var firstName = "Rashedul";
+// var lastName = "Hasan";
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+// var num = Math.abs(-324);
+// console.log(num);
+
+// var num2 = Math.round(3.5);
+// console.log(num2);
+
+// var num3 = Math.floor(4.567);
+// console.log(num3);
+
+// var num4 = Math.ceil(4.567);
+// console.log(num4);
+
+// var num5 = Math.random()*10;
+// var num6 = Math.ceil(num5);
+// console.log(num6);
+
+// var mangoPrice = 20;
+// if(mangoPrice != 20){
+//     console.log("You can buy mango");
+// }
+// else{
+//     console.log("You can't buy mango");
+// }
+
+
+// var numberOneFriend = 100;
+// var numberTwoFriend = 50;
+// if(numberOneFriend == 100 && numberTwoFriend == 40){
+//     console.log("Go to Rastaurant-01");
+// }
+// else if(numberOneFriend >= 100 || numberTwoFriend >= 100){
+//     console.log("Go to Rastaurant-02");
+// }
+// else{
+//     console.log("Can't go to Rastaurant");
+// }
